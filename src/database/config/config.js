b6af48c6a4529753d7f522bc85b6b56d68f3a3bb
gabrielsbiche <gabrielsbiche@gmail.com>
2022-09-15 10:50:37 -0300

    feat(database): added environment variables and applied these variables in the database configuration

---
+++ src/database/config/config.js
@@ -0,0 +1,38 @@
+require('dotenv/config.js');
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: mysql,
+    dialectOptions: {
+      bigNumberStrings: true,
+    },
+  },
+  test: {
+    username: '',
+    password: '',
+    database: '',
+    host: '',
+    port: '',
+    dialect: '',
+    dialectOptions: {
+      bigNumberStrings: true,
+    },
+  },
+  production: {
+    username: '',
+    password: '',
+    database: '',
+    host: '',
+    port: '',
+    dialect: '',
+    dialectOptions: {
+      bigNumberStrings: true,
+    },
+  },
+};
+
