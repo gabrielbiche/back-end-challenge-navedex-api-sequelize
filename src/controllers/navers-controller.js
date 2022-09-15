227147f709947e194dee12845095dd305dc14957
gabrielsbiche <gabrielsbiche@gmail.com>
2022-09-15 17:18:34 -0300

    feat(controllers): removed try catch blocks due to error handling middleware implementation

--- src/controllers/navers-controller.js
+++ src/controllers/navers-controller.js
@@ -1,144 +1,133 @@
-import { NotFound, Unauthorized } from '../helpers/errors'
+import { NotFound, Unauthorized } from '../helpers'
 import db from '../models'
 
 const Navers = db.Navers
 const Projects = db.Projects
 
-
-export const index = async(req, res) => {
+export const index = async (req, res) => {
   const { user_id } = req.params
   const { name, admission_date, job_role } = req.query
-  try {
-    const where = { user_id }
-    if (name) where.name = name
-    if (admission_date) where.admission_date = admission_date
-    if (job_role) where.job_role = job_role
-    const navers = await Navers.findAll({
-      where,
-      attributes: ['id', 'name', 'birthdate', 'admission_date', 'job_role']
-    })
-    return res.status(200).json(navers)
-  } catch (error) {
-    return res.status(500).json({ Message: error.message })
-  }
+
+  const where = { user_id }
+  if (name) where.name = name
+  if (admission_date) where.admission_date = admission_date
+  if (job_role) where.job_role = job_role
+
+  const navers = await Navers.findAll({
+    where,
+    attributes: ['id', 'name', 'birthdate', 'admission_date', 'job_role']
+  })
+
+  return res.status(200).json(navers)
 }
 
-export const show = async(req, res) => {
+export const show = async (req, res) => {
   const { user_id, naver_id } = req.params
-  try {
-    const naverAndProjects = await Navers.findOne({
-      where: { id: naver_id, user_id: user_id },
-      attributes: ['id', 'name', 'birthdate', 'admission_date', 'job_role'],
-      include: {
-        association: 'projects',
-        attributes: ['id', 'name'],
-        through: { attributes: [] }
-      }
-    })
-    return res.status(200).json(naverAndProjects)
-  } catch (error) {
-    return res.status(500).json({ Message: error.message })
-  }
+
+  const naverAndProjects = await Navers.findOne({
+    where: { id: naver_id, user_id: user_id },
+    attributes: ['id', 'name', 'birthdate', 'admission_date', 'job_role'],
+    include: {
+      association: 'projects',
+      attributes: ['id', 'name'],
+      through: { attributes: [] }
+    }
+  })
+  
+  return res.status(200).json(naverAndProjects)
 }
 
-export const store = async(req, res) => {
+export const store = async (req, res) => {
   const { user_id } = req.params
   const { name, birthdate, admission_date, job_role, projects } = req.body
-  try {
-    if (projects) {
-      const project = await Projects.findAll({
-        where: { id: projects }
-      })
-      if (!project || project.length < projects.length)
-        throw new NotFound('Project not found')
-    }
-    const naver = await Navers.create({
-      name,
-      user_id,
-      birthdate,
-      admission_date,
-      job_role
+
+  if (projects) {
+    const project = await Projects.findAll({
+      where: { id: projects }
     })
-    if (projects) await naver.setProjects(projects)
-    const naverAndProjects = await Navers.findByPk(naver.id, {
-      attributes: ['name', 'birthdate', 'admission_date', 'job_role'],
-      association: 'projects',
-      include: {
-        model: Projects,
-        association: 'projects',
-        attributes: ['id'],
-        through: { attributes: [] }
-      }
-    })
-    return res.status(201).json(naverAndProjects)
-  } catch (error) {
-    if (error && error instanceof NotFound) {
-      return res.status(error.statusCode).json({ Message: error.message })
-    } else if (error && error.name === 'SequelizeValidationError') {
-      return res.status(400).json({ Message: error.message })
+    if (!project || project.length < projects.length) {
+      throw new NotFound('Project not found')
     }
-    res.status(500).json({ Message: error.message })
   }
+
+  const naver = await Navers.create({
+    name,
+    user_id,
+    birthdate,
+    admission_date,
+    job_role
+  })
+
+  if (projects) await naver.setProjects(projects)
+
+  const naverAndProjects = await Navers.findByPk(naver.id, {
+    attributes: ['name', 'birthdate', 'admission_date', 'job_role'],
+    association: 'projects',
+    include: {
+      model: Projects,
+      association: 'projects',
+      attributes: ['id'],
+      through: { attributes: [] }
+    }
+  })
+
+  return res.status(201).json(naverAndProjects)
 }
 
-export const update = async(req, res) => {
+export const update = async (req, res) => {
   const { user_id, naver_id } = req.params
   const { name, birthdate, admission_date, job_role, projects } = req.body
-  try {
-    const naver = await Navers.findOne({
-      where: { id: naver_id, user_id: user_id }
-    })
-    if (!naver)
-      throw new Unauthorized(`User id ${user_id} does not have naver id ${naver_id}`)
-    if (projects) {
-      const project = await Projects.findAll({
-        where: { id: projects }
-      })
-      if (!project || project.length < projects.length)
-        throw new NotFound('Project not found')
-    }
-    await Navers.update(
-      { name, user_id, birthdate, admission_date, job_role },
-      { where: { id: naver.id } }
-    )
-    if (projects) await naver.setProjects(projects)
-    const updatedNaverAndProjects = await Navers.findByPk(naver.id, {
-      attributes: ['name', 'birthdate', 'admission_date', 'job_role'],
-      include: {
-        association: 'projects',
-        attributes: ['id'],
-        through: { attributes: [] }
-      }
-    })
-    return res.status(200).json(updatedNaverAndProjects)
-  } catch (error) {
-    if (error && error instanceof NotFound) {
-      return res.status(error.statusCode).json({ Message: error.message })
-    } else if (error && error instanceof Unauthorized) {
-      return res.status(error.statusCode).json({ Message: error.message })
-    } else if (error && error.name === 'SequelizeValidationError') {
-      return res.status(400).json({ Message: error.message })
-    }
-    res.status(500).json({ Message: error.message })
+
+  const naver = await Navers.findOne({
+    where: { id: naver_id, user_id: user_id }
+  })
+
+  if (!naver) {
+    throw new Unauthorized(`User id ${user_id} does not have naver id ${naver_id}`)
   }
+
+  if (projects) {
+    const project = await Projects.findAll({
+      where: { id: projects }
+    })
+
+    if (!project || project.length < projects.length) {
+      throw new NotFound('Project not found')
+    } 
+  }
+
+  await Navers.update(
+    { name, user_id, birthdate, admission_date, job_role },
+    { where: { id: naver.id } }
+  )
+
+  if (projects) await naver.setProjects(projects)
+
+  const updatedNaverAndProjects = await Navers.findByPk(naver.id, {
+    attributes: ['name', 'birthdate', 'admission_date', 'job_role'],
+    include: {
+      association: 'projects',
+      attributes: ['id'],
+      through: { attributes: [] }
+    }
+  })
+  return res.status(200).json(updatedNaverAndProjects)
 }
 
-export const destroy = async(req, res) => {
+export const destroy = async (req, res) => {
   const { user_id, naver_id } = req.params
-  try {
-    const naver = await Navers.findOne({
-      where: { id: naver_id, user_id: user_id }
-    })
-    if (!naver)
-      throw new Unauthorized(`User id ${user_id} does not have naver id ${naver_id}`)
-    await Navers.destroy({ where: { id: naver.id } })
-    return res.status(204).end()
-  } catch (error) {
-    if (error && error instanceof Unauthorized) {
-      return res.status(error.statusCode).json({ Message: error.message })
-    }
-    res.status(500).json({ Message: error.message })
+
+  const naver = await Navers.findOne({
+    where: { id: naver_id, user_id: user_id }
+  })
+
+  if (!naver) {
+    throw new Unauthorized(`User id ${user_id} does not have naver id ${naver_id}`)
   }
+
+  await Navers.destroy({ where: { id: naver.id } })
+  
+  return res.status(204).end()
 }
 
 export default {
--- src/controllers/projects-controller.js
+++ src/controllers/projects-controller.js
@@ -1,5 +1,5 @@
-import { NotFound, Unauthorized } from "../helpers/errors"
-import db from "../models"
+import { NotFound, Unauthorized } from '../helpers'
+import db from '../models'
 
 const Projects = db.Projects
 const Navers = db.Navers
@@ -7,124 +7,111 @@ const Navers = db.Navers
 export const index = async (req, res) => {
   const { user_id } = req.params
   const { name } = req.query
-  try {
-    const where = { user_id }
-    if (name) where.name = name
-    const projects = await Projects.findAll({
-      where,
-      attributes: ["id", "name"]
-    })
-    return res.status(200).json(projects)
-  } catch (error) {
-    return res.status(500).json({ Message: error.message })
-  }
+
+  const where = { user_id }
+  if (name) where.name = name
+  const projects = await Projects.findAll({
+    where,
+    attributes: ['id', 'name']
+  })
+
+  return res.status(200).json(projects)
 }
 
 export const show = async (req, res) => {
   const { user_id, project_id } = req.params
-  try {
-    const projectAndNavers = await Projects.findOne({
-      where: { id: project_id, user_id: user_id },
-      attributes: ["id", "name"],
-      include: {
-        association: "navers",
-        attributes: ["id", "name", "birthdate", "admission_date", "job_role"],
-        through: { attributes: [] }
-      }
-    })
-    return res.status(200).json(projectAndNavers)
-  } catch (error) {
-    return res.status(500).json({ Message: error.message })
-  }
+
+  const projectAndNavers = await Projects.findOne({
+    where: { id: project_id, user_id: user_id },
+    attributes: ['id', 'name'],
+    include: {
+      association: 'navers',
+      attributes: ['id', 'name', 'birthdate', 'admission_date', 'job_role'],
+      through: { attributes: [] }
+    }
+  })
+
+  return res.status(200).json(projectAndNavers)
 }
 
 export const store = async (req, res) => {
   const { user_id } = req.params
   const { name, navers } = req.body
-  try {
-    if (navers) {
-      const naver = await Navers.findAll({ where: { id: navers } })
-      if (!naver || naver.length < navers.length)
-        throw new NotFound("Naver not found")
+
+  if (navers) {
+    const naver = await Navers.findAll({ where: { id: navers } })
+    
+    if (!naver || naver.length < navers.length) {
+      throw new NotFound('Naver not found')
     }
-    const project = await Projects.create({ name, user_id })
-    if (navers) await project.setNavers(navers)
-    const projectAndNavers = await Projects.findByPk(project.id, {
-      attributes: ["name"],
-      include: {
-        association: "navers",
-        attributes: ["id"],
-        through: { attributes: [] }
-      }
-    })
-    return res.status(201).json(projectAndNavers)
-  } catch (error) {
-    if (error && error instanceof NotFound) {
-      return res.status(error.statusCode).json({ Message: error.message })
-    } else if (error && error.name === "SequelizeValidationError") {
-      return res.status(400).json({ Message: error.message })
-    }
-    res.status(500).json({ Message: error.message })
   }
+  
+  const project = await Projects.create({ name, user_id })
+  
+  if (navers) await project.setNavers(navers)
+  
+  const projectAndNavers = await Projects.findByPk(project.id, {
+    attributes: ['name'],
+    include: {
+      association: 'navers',
+      attributes: ['id'],
+      through: { attributes: [] }
+    }
+  })
+  
+  return res.status(201).json(projectAndNavers)
 }
 
 export const update = async (req, res) => {
   const { user_id, project_id } = req.params
   const { name, navers } = req.body
-  try {
-    const project = await Projects.findOne({
-      where: { id: project_id, user_id: user_id },
-    })
-    if (!project)
-      throw new Unauthorized(
-        `User id ${user_id} does not have project id ${project_id}`
-      )
-    if (navers) {
-      const naver = await Navers.findAll({ where: { id: navers } })
-      if (!naver || naver.length < navers.length)
-        throw new NotFound("Naver not found")
-    }
-    await Projects.update({ name: name }, { where: { id: project.id } })
-    if (navers) await project.setNavers(navers)
-    const updatedProjectAndNavers = await Projects.findByPk(project.id, {
-      attributes: ["name"],
-      include: {
-        association: "navers",
-        attributes: ["id"],
-        through: { attributes: [] }
-      }
-    })
-    return res.status(200).json(updatedProjectAndNavers);
-  } catch (error) {
-    if (error && error instanceof NotFound) {
-      return res.status(error.statusCode).json({ Message: error.message })
-    } else if (error && error instanceof Unauthorized) {
-      return res.status(error.statusCode).json({ Message: error.message })
-    } else if (error && error.name === "SequelizeValidationError") {
-      return res.status(400).json({ Message: error.message })
-    }
-    res.status(500).json({ Message: error.message })
+
+  const project = await Projects.findOne({
+    where: { id: project_id, user_id: user_id }
+  })
+
+  if (!project) {
+    throw new Unauthorized(`User id ${user_id} does not have project id ${project_id}`)
   }
+
+  if (navers) {
+    const naver = await Navers.findAll({ where: { id: navers } })
+    
+    if (!naver || naver.length < navers.length) {
+      throw new NotFound('Naver not found')
+    }
+  }
+
+  await Projects.update({ name: name }, { where: { id: project.id } })
+  
+  if (navers) await project.setNavers(navers)
+
+  const updatedProjectAndNavers = await Projects.findByPk(project.id, {
+    attributes: ['name'],
+    include: {
+      association: 'navers',
+      attributes: ['id'],
+      through: { attributes: [] }
+    }
+  })
+
+  return res.status(200).json(updatedProjectAndNavers)
 }
 
 export const destroy = async (req, res) => {
   const { user_id, project_id } = req.params
-  try {
-    const project = await Projects.findOne({
-      where: { id: project_id, user_id: user_id },
-    });
-    if (!project)
-      throw new Unauthorized(
-        `User id ${user_id} does not have project id ${project_id}`
-      )
-    await Projects.destroy({ where: { id: project.id } })
-    return res.status(204).end()
-  } catch (error) {
-    if (error && error instanceof Unauthorized) {
-      return res.status(error.statusCode).json({ Message: error.message })
-    }
-    res.status(500).json({ Message: error.message })
+
+  const project = await Projects.findOne({
+    where: { id: project_id, user_id: user_id }
+  })
+
+  if (!project) {
+    throw new Unauthorized(`User id ${user_id} does not have project id ${project_id}`)
   }
+
+  await Projects.destroy({ where: { id: project.id } })
+  
+  return res.status(204).end()
 }
 
 export default {
