2b4a6d67c8011c677a8ce60a2d26283f91bc73a3
gabrielsbiche <gabrielsbiche@gmail.com>
2022-09-16 15:39:27 -0300

    feat(environment): added added default values for environment variables

--- src/config.js
+++ src/config.js
@@ -5,11 +5,11 @@ dotenv.config({
   path: path.resolve(__dirname, '../.env')
 })
 
-export const PORT = process.env.PORT
-export const ACCESS_SECRET = process.env.ACCESS_SECRET
-export const ACCESS_EXPIRES = process.env.ACCESS_EXPIRES
-export const DB_USER = process.env.DB_USER
-export const DB_PASSWORD = process.env.DB_PASSWORD
-export const DB_HOST = process.env.DB_HOST
-export const DB_PORT = process.env.DB_PORT
-export const DB_DATABASE = process.env.DB_DATABASE
+export const PORT = process.env.PORT || 3000
+export const ACCESS_SECRET = process.env.ACCESS_SECRET || 'mysupersecret'
+export const ACCESS_EXPIRES = process.env.ACCESS_EXPIRES || 1200
+export const DB_USER = process.env.DB_USER || 'root'
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'root'
+export const DB_HOST = process.env.DB_HOST || 'localhost'
+export const DB_PORT = process.env.DB_PORT || 3306
+export const DB_DATABASE = process.env.DB_DATABASE || 'database_test'
